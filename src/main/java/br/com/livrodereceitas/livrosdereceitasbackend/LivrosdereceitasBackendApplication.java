1da157e70ad539e82134203ad1778ed62ca6b40e
claytoncalixto <claytonrcalixto@gmail.com>
2021-04-06 20:00:02 -0300

    First Commit #1

---
+++ src/main/java/br/com/livrodereceitas/livrosdereceitasbackend/LivrosdereceitasBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.livrodereceitas.livrosdereceitasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrosdereceitasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrosdereceitasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/livrodereceitas/livrosdereceitasbackend/LivrosdereceitasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.livrodereceitas.livrosdereceitasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrosdereceitasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
