c8e1941194d1df3878151864345dafbada0772d3
claytoncalixto <claytonrcalixto@gmail.com>
2021-04-07 20:08:40 -0300

    Aula 11 - Creation Rest #1

--- src/main/java/br/com/livrodereceitas/livrosdereceitasbackend/LivrodereceitasBackendApplication.java
+++ src/main/java/br/com/livrodereceitas/livrosdereceitasbackend/LivrodereceitasBackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LivrosdereceitasBackendApplication {
+public class LivrodereceitasBackendApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LivrosdereceitasBackendApplication.class, args);
+		SpringApplication.run(LivrodereceitasBackendApplication.class, args);
 	}
 
 }
---
+++ src/main/java/br/com/livrodereceitas/livrosdereceitasbackend/web/rest/IndexController.java
@@ -0,0 +1,22 @@
+package br.com.livrodereceitas.livrosdereceitasbackend.web.rest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    private final Logger log = LoggerFactory.getLogger(IndexController.class);
+
+    @GetMapping
+    public ResponseEntity<String> getIndex() {
+        log.info("Teste de log debug");
+        return new ResponseEntity<>("Livro de Receitas", HttpStatus.OK);
+    }
+}
